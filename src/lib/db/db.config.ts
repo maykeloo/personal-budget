eab180fa367b32027aa8823a05edb8b49f5a5db7
Michał Sukiennik <michal.sukiennik.contact@gmail.com>
2024-11-10 23:29:46 +0100

    refactor: update Supabase environment variable names to VITE prefix

--- src/lib/db/db.config.ts
+++ src/lib/db/db.config.ts
@@ -2,8 +2,8 @@ import type { Database } from '@/schema'
 import { Env } from '@/shared/types/env'
 import { createClient } from '@supabase/supabase-js'
 
-const { SUPABASE_URL, SUPABASE_KEY } = Env.variables()
+const { VITE_SUPABASE_KEY, VITE_SUPABASE_URL } = Env.variables()
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_KEY)
 
 export type DatabaseClient = typeof supabase
--- src/shared/types/env.ts
+++ src/shared/types/env.ts
@@ -5,26 +5,14 @@ const nodeEnv = z.object({
 })
 
 const envSchema = z.object({
-  SUPABASE_URL: z.string(),
-  SUPABASE_KEY: z.string(),
+  VITE_SUPABASE_URL: z.string(),
+  VITE_SUPABASE_KEY: z.string(),
 })
-
-const devEnvSchema = z.object(
-  Object.fromEntries(Object.entries(envSchema.shape).map(([key, type]) => [`VITE_${key}`, type])),
-)
 
 type EnvSchema = z.infer<typeof envSchema>
 
 export class Env {
   static variables() {
-    console.log(import.meta.env)
-    if (nodeEnv.parse(import.meta.env).MODE === 'development') {
-      const developmentSchema = devEnvSchema.parse(import.meta.env)
-      return {
-        SUPABASE_KEY: developmentSchema.VITE_SUPABASE_KEY,
-        SUPABASE_URL: developmentSchema.VITE_SUPABASE_URL,
-      }
-    }
     return envSchema.parse(import.meta.env)
   }
 }
