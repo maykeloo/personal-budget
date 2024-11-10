a6f3b3e3cb0842c00984f1a01c8924644c1ed1b6
Michał Sukiennik <michal.sukiennik.contact@gmail.com>
2024-11-10 23:47:31 +0100

    refactor: update router history configuration, set base path in Vite, and add Vercel configuration for rewrites

--- src/lib/router/index.ts
+++ src/lib/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routerMiddleware } from '@/lib/router/middlewares/auth.middleware'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
