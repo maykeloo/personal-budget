35d2ce03913d10cefca9d577057fd3958e7c07b9
Michał Sukiennik <michal.sukiennik.contact@gmail.com>
2024-11-11 10:48:40 +0100

    refactor: remove unused web hash history import from router configuration

--- src/lib/router/index.ts
+++ src/lib/router/index.ts
@@ -1,9 +1,8 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { routerMiddleware } from '@/lib/router/middlewares/auth.middleware'
 import FeatureSignIn from '@/features/auth/sign-in/feature-sign-in.vue'
 import FeatureSignUp from '@/features/auth/sign-up/feature-sign-up.vue'
 import FeatureDashboard from '@/features/dashboard/feature-dashboard.vue'
-import About from '@/About.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
